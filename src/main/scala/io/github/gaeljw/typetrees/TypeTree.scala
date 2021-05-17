7a1a63698f118e907191f75461c656b85715fedb
Gaël Jourdan-Weil <gael.jourdan-weil@kelkoogroup.com>
2021-05-17 11:31:40 +0200

    style: 💄 Do apply scalafmt

--- src/main/scala/io/github/gaeljw/typetrees/TypeTree.scala
+++ src/main/scala/io/github/gaeljw/typetrees/TypeTree.scala
@@ -11,19 +11,49 @@ trait TypeTree[T] {
 
 object TypeTree {
 
-  given typeTree4[T[A, B, C, D], A: TypeTree, B:TypeTree, C:TypeTree, D:TypeTree](using ClassTag[T[A, B, C, D]]): TypeTree[T[A, B, C, D]] with {
-    def tag = TypeTreeTag(summon[ClassTag[T[A, B, C, D]]], List(summon[TypeTree[A]].tag, summon[TypeTree[B]].tag, summon[TypeTree[C]].tag, summon[TypeTree[D]].tag))
+  given typeTree4[T[
+      A,
+      B,
+      C,
+      D
+  ], A: TypeTree, B: TypeTree, C: TypeTree, D: TypeTree](using
+      ClassTag[T[A, B, C, D]]
+  ): TypeTree[T[A, B, C, D]] with {
+    def tag = TypeTreeTag(
+      summon[ClassTag[T[A, B, C, D]]],
+      List(
+        summon[TypeTree[A]].tag,
+        summon[TypeTree[B]].tag,
+        summon[TypeTree[C]].tag,
+        summon[TypeTree[D]].tag
+      )
+    )
   }
 
-  given typeTree3[T[A, B, C], A: TypeTree, B:TypeTree, C:TypeTree](using ClassTag[T[A, B, C]]): TypeTree[T[A, B, C]] with {
-    def tag = TypeTreeTag(summon[ClassTag[T[A, B, C]]], List(summon[TypeTree[A]].tag, summon[TypeTree[B]].tag, summon[TypeTree[C]].tag))
+  given typeTree3[T[A, B, C], A: TypeTree, B: TypeTree, C: TypeTree](using
+      ClassTag[T[A, B, C]]
+  ): TypeTree[T[A, B, C]] with {
+    def tag = TypeTreeTag(
+      summon[ClassTag[T[A, B, C]]],
+      List(
+        summon[TypeTree[A]].tag,
+        summon[TypeTree[B]].tag,
+        summon[TypeTree[C]].tag
+      )
+    )
   }
 
-  given typeTree2[T[A, B], A: TypeTree, B: TypeTree](using ClassTag[T[A, B]]): TypeTree[T[A, B]] with {
-    def tag = TypeTreeTag(summon[ClassTag[T[A, B]]], List(summon[TypeTree[A]].tag, summon[TypeTree[B]].tag))
+  given typeTree2[T[A, B], A: TypeTree, B: TypeTree](using
+      ClassTag[T[A, B]]
+  ): TypeTree[T[A, B]] with {
+    def tag = TypeTreeTag(
+      summon[ClassTag[T[A, B]]],
+      List(summon[TypeTree[A]].tag, summon[TypeTree[B]].tag)
+    )
   }
 
-  given typeTree1[T[A], A : TypeTree](using ClassTag[T[A]]): TypeTree[T[A]] with {
+  given typeTree1[T[A], A: TypeTree](using ClassTag[T[A]]): TypeTree[T[A]]
+    with {
     def tag = TypeTreeTag(summon[ClassTag[T[A]]], List(summon[TypeTree[A]].tag))
   }
 
